fce7b5405327d052e8e2549165c7780fb3745674
Misha Sahan <mishkasahan@gmail.com>
2023-12-19 20:56:09 +0200

    DeleteLines

--- main.py
+++ main.py
@@ -1,31 +1,17 @@
-class Vehicle :
-    def __init__(self, name_car: str, max_speed: int, price: int):
-        self.name_car = name_car
-        self.max_speed = max_speed
-        self.price = price
-
-    def __gt__(self, other):
-        return self.max_speed > other.max_speed
-
-    def __str__(self):
-        return f"{self.name_car}, {self.max_speed}"
-
-
-def sortt(array):
-    length = len(array)
-    for i in range(0, length):
-        for j in range(0, length - i - 1):
-            if array[j] > array[j + 1]:
-                temp = array[j]
-                array[j] = array[j + 1]
-                array[j + 1] = temp
+def delete_line_from_file(file_path, line_to_delete):
+    try:
+        with open(file_path, 'r', encoding= 'UTF-8') as file:
+            lines = file.readlines()
+            lines = [line for line in lines if line.strip() != line_to_delete.strip()]
+        with open(file_path, 'w', encoding= 'UTF-8') as file:
+            file.writelines(lines)
+            print(f"Рядок '{line_to_delete}' був успішно видалений з файлу.")
+    except FileNotFoundError:
+        print(f"Файл '{file_path}' не знайдено.")
+    except Exception as e:
+        print(f"Виникла помилка: {str(e)}")
 
 
-mers = Vehicle("mers", 220,30000)
-bmw = Vehicle('BMW', 230,35000)
-audi = Vehicle("AUDI", 200, 25000)
-
-b = [mers,bmw,audi]
-sortt(b)
-for i in b:
-    print(i)
+file_path = 'file_path.txt'
+line_to_delete = 'привіт'
+delete_line_from_file(file_path, line_to_delete)
